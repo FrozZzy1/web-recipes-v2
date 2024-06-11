2ef0931fd6f1c10484c01ac32109de1767a5eeb7
FrozZzy1 <frozzzy71@gmail.com>
2024-06-11 09:46:03 +0700

    feat: add db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+
+# from app.database.db import delete_tables
+from app.database.db import create_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # await delete_tables()
+    await create_tables()
+    yield
+
 
 app = FastAPI(
     title='Рецепты',
+    lifespan=lifespan,
 )
 
 
