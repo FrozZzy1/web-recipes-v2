6c0e6b0590ad492b88ef804a365721762a31e2c1
FrozZzy1 <frozzzy71@gmail.com>
2024-06-18 03:59:30 +0700

    feat: add migrations

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-# from app.database.db import delete_tables
+from app.database.db import delete_tables
 from app.database.db import create_tables
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # await delete_tables()
+    await delete_tables()
     await create_tables()
     yield
 
